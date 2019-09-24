a95d33aee6b929fe66f19755d63e9bbe1b4050ab
John Pinkerton <johnpinker@yahoo.com>
2019-09-24 07:55:39 -0500

    added logging and fixed update bug

--- app/routes/links_routes.js
+++ app/routes/links_routes.js
@@ -3,6 +3,7 @@ const ObjectID = require("mongodb").ObjectID;
 
 module.exports = function(app, db) {  
     
+    // Get one
     app.get("/links/:id", (req, res) => {
         const id = req.params.id;
         const details = { _id: new ObjectID(id) }
@@ -10,45 +11,55 @@ module.exports = function(app, db) {
             if (err) {
                 res.send({error: "An Error has occured."});
             } else {
+                console.log("Fetching item: " + item);
                 res.send(item);
             }
         });
     });
 
+    // get all
     app.get("/links", (req, res) => {
         db.collection('links').find({}).toArray((err, result) => {
             if (err) { 
                 res.send({'error': 'An Error has occured.'});
             } else {
+                console.log("Fetching all.")
                 res.send(result);
             }
         });
     })
     
+    // insert one
     app.post('/links', (req, res) => {    
         const note = { href: req.body.href, name: req.body.name };    
         db.collection('links').insertOne(note, (err, result) => {      
                 if (err) {         
                     res.send({ 'error': 'An error has occurred' });       
-                } else {        
+                } else {
+                    console.log("Inserting one: id: " + result.ops[0]);       
                     res.send(result.ops[0]);      
                 }    
         });  
     });
 
+    // update one
     app.put('/links/:id', (req, res) => {
         const id = req.params.id;
         const details = {_id: new ObjectID(id) }
-        const note = {name: req.body.name, href: req.body.href }
+        const note = {$set: {name: req.body.name, href: req.body.href }};
         db.collection('links').updateOne(details, note, (err, result) => {
             if (err) {
+                console.log(err);
+                console.log(note);
                 res.send({ 'error': 'An error has occurred' });  
             } else {
+                console.log("updating: " + note);
                 res.send(note);
             }
         });
     });
 
+    // delete one
     app.delete('/links/:id', (req, res) => {
         const id = req.params.id;
         const details = { _id: new ObjectID(id) }
@@ -56,6 +67,7 @@ module.exports = function(app, db) {
             if (err) {
                 res.send({ 'error': 'An error has occurred' });  
             } else {
+                console.log("deleting: " + details);
                 res.send(details);
             }
         });
--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app            = express();
 const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
 MongoClient.connect(db.url, (err, database) => {  
     if (err) return console.log(err)                        
     // Make sure you add the database name and not the collection name  
